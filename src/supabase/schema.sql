7670cbb3763fa00c3db848eb4877bd4d57c37af1
Aryan Thopate <aryanthopate52@gmail.com>
2025-10-19 17:08:36 +0000

    ERROR:  42723: function "handle_new_user" already exists with same argum

--- src/supabase/schema.sql
+++ src/supabase/schema.sql
@@ -1,34 +1,16 @@
 -- Create a table for public profiles
 create table profiles (
-  id uuid not null references auth.users on delete cascade,
-  email text,
-  full_name text,
-  learning_at text,
+  id uuid references auth.users on delete cascade not null primary key,
   updated_at timestamp with time zone,
-
-  primary key (id)
+  full_name text,
+  email text,
+  learning_at text
 );
 
+-- Set up Row Level Security (RLS)
+-- See https://supabase.com/docs/guides/auth/row-level-security
 alter table profiles
   enable row level security;
-
--- inserts a row into public.profiles
-create function public.handle_new_user()
-returns trigger
-language plpgsql
-security definer set search_path = public
-as $$
-begin
-  insert into public.profiles (id, email, full_name, learning_at)
-  values (new.id, new.email, new.raw_user_meta_data->>'full_name', new.raw_user_meta_data->>'learning_at');
-  return new;
-end;
-$$;
-
--- trigger the function every time a user is created
-create trigger on_auth_user_created
-  after insert on auth.users
-  for each row execute procedure public.handle_new_user();
 
 create policy "Public profiles are viewable by everyone." on profiles
   for select using (true);
@@ -38,6 +20,27 @@ create policy "Users can insert their own profile." on profiles
 
 create policy "Users can update own profile." on profiles
   for update using (auth.uid() = id);
+
+-- This trigger automatically creates a profile for new users.
+-- See https://supabase.com/docs/guides/auth/managing-user-data#using-triggers
+DROP FUNCTION IF EXISTS public.handle_new_user();
+create function public.handle_new_user()
+returns trigger
+language plpgsql
+security definer set search_path = public
+as $$
+begin
+  insert into public.profiles (id, full_name, email, learning_at)
+  values (new.id, new.raw_user_meta_data->>'full_name', new.email, new.raw_user_meta_data->>'learning_at');
+  return new;
+end;
+$$;
+
+-- trigger the function every time a user is created
+DROP TRIGGER IF EXISTS on_auth_user_created ON auth.users;
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
 
 -- Set up Realtime!
 begin;
