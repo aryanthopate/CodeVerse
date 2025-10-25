569a1684bb067f1a05046ac91421a3fa225ed930
Aryan Thopate <aryanthopate52@gmail.com>
2025-10-25 08:34:49 +0000

    on homepage still img not showing

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@
 import type {NextConfig} from 'next';
 require('dotenv').config({ path: './.env' });
 
+const supabaseHostname = process.env.NEXT_PUBLIC_SUPABASE_URL
+  ? new URL(process.env.NEXT_PUBLIC_SUPABASE_URL).hostname
+  : undefined;
+
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
@@ -14,6 +18,18 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      ...(supabaseHostname ? [{
+          protocol: 'https',
+          hostname: supabaseHostname,
+        } as const] : []),
+    ],
+  }
 };
 
 export default nextConfig;
